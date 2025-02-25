cdaf0e5d62a5d1325562241a531df93c2450fcab
oneShot0430 <healer0991@gmail.com>
2025-02-24 23:06:35 -0800

    dont allow to visible env file

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,9 +2,9 @@
 import { CMCResult } from "@/type/interface";
 
 export const USDC = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-export const RANGO_API_KEY = "c6381a79-2817-4602-83bf-6a641a409e32";
-export const CMC_API_KEY = "035d8ca6-8c6c-4395-8869-e6a1952ecede";
-export const HELIUS_API_KEY = "f72d8256-c91e-452e-9c07-bb59dace8f21";
+export const RANGO_API_KEY = import.meta.env.VITE_RANGO_API_KEY;
+export const CMC_API_KEY = import.meta.env.VITE_CMC_API_KEY;
+export const HELIUS_API_KEY = import.meta.env.VITE_HELIUS_API_KEY;
 
 export const BRETT = {
   symbol: 'BRETT', 
